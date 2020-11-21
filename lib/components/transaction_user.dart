ce6b62fee2405648dc009f4f1f7c19a758ae2fe7
gabriel <gabriellopes112009@gmail.com>
2020-11-21 18:53:20 -0300

    transaction submition initial

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:despesa_pessoal/models/transaction.dart';
 import 'package:flutter/cupertino.dart';
 import 'transaction_form.dart';
@@ -25,12 +27,26 @@ class _TransactionUserState extends State<TransactionUser> {
     ),
   ];
   
+  _addTransaction (String title, double value){
+    final newTransaction = Transaction(
+      id : Random().nextDouble().toString(),
+      title: title,
+      value : value,
+      date : DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         TransactionList(_transactions),
-        TransactionForm(),
+        TransactionForm(_addTransaction),
       ],
       
     );
