ca55db120271346595b22751165a7639a75e0e0b
gabriel <gabriellopes112009@gmail.com>
2020-11-22 13:24:47 -0300

    add image

--- lib/main.dart
+++ lib/main.dart
@@ -44,19 +44,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo tênis',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Cinema',
-      value: 211.30,
-      date: DateTime.now(),
-    )
+  final List<Transaction> _transactions = [
+    
+    // Transaction(
+    //   id: 't1',
+    //   title: 'Novo tênis',
+    //   value: 310.76,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Cinema',
+    //   value: 211.30,
+    //   date: DateTime.now(),
+    // )
   ];
 
   _addTransaction(String title, double value) {
