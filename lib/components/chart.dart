348693693e81a4a156e2f02e80a99b2072273eb4
gabriel <gabriellopes112009@gmail.com>
2020-11-22 15:23:21 -0300

    initial chart implementation

---
+++ lib/components/chart.dart
@@ -0,0 +1,48 @@
+import 'package:despesa_pessoal/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalSum = 0.0;
+
+      for(var i = 0; i < recentTransactions.length; i++){
+        bool sameDay = recentTransactions[i].date.day == weekDay.day;
+        bool sameMonth = recentTransactions[i].date.month == weekDay.month;
+        bool sameYear = recentTransactions[i].date.month == weekDay.year;
+
+        if(sameDay && sameMonth && sameYear){
+          totalSum += recentTransactions[i].value;
+        }
+      }
+
+      print(DateFormat.E().format(weekDay)[0]);
+      print(totalSum);
+
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
+        'value': 9.99,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
