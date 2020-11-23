db7d68a1c81500ca9390de9afcfc89b93fe7ce35
gabriel <gabriellopes112009@gmail.com>
2020-11-23 18:41:29 -0300

    refactoring initial screen

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -47,16 +47,23 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),      
-      child: Row(
-        children: groupedTransactions.map(
-          (tr) {
-            return ChartBar(
-              label: tr['day'],
-              value: tr['value'],
-              percentage: (tr['value'] as double) / _weekTotalValue,
-            );
-          }
-        ).toList(),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactions.map(
+            (tr) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: ChartBar(
+                  label: tr['day'],
+                  value: tr['value'],
+                  percentage: (tr['value'] as double) / _weekTotalValue,
+                ),
+              );
+            }
+          ).toList(),
+        ),
       ),
     );
   }
