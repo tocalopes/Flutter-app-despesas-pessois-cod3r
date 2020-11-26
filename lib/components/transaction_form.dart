7e3ae65934ac7bc2b91cb95bc8b8c4516c8818f7
gabriel <gabriellopes112009@gmail.com>
2020-11-26 11:33:58 -0300

    easy fixed show keyboard on transaction form

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -20,7 +20,7 @@ class _TransactionFormState extends State<TransactionForm> {
 
   _submitForm() {
     final title = _titleController.text;
-    final value = double.tryParse(_valueController.text) ?? 0.0;
+    final value = double.tryParse(_valueController.text.replaceAll(',', '.')) ?? 0.0;
 
     if (title.isEmpty || value <= 0 || _selectedDate == null) {
       return;
@@ -47,64 +47,71 @@ class _TransactionFormState extends State<TransactionForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _titleController,
-              decoration: InputDecoration(
-                labelText: 'Título',
+    return SingleChildScrollView(
+        child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 10,
+            right: 10,
+            left: 10,
+            bottom: 10 + MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Column(
+            children: [
+              TextField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  labelText: 'Título',
+                ),
+                onSubmitted: (_) => _submitForm(),
               ),
-              onSubmitted: (_) => _submitForm(),
-            ),
-            TextField(
-              controller: _valueController,
-              decoration: InputDecoration(
-                labelText: 'Valor R\$',
+              TextField(
+                controller: _valueController,
+                decoration: InputDecoration(
+                  labelText: 'Valor R\$',
+                ),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onSubmitted: (_) => _submitForm(),
               ),
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_) => _submitForm(),
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: [
-                  Expanded(
-                      child: Text(
-                      _selectedDate == null ? 'Nenhuma data selecionada'
-                      : 'Data: ${DateFormat('dd/MM/yyyy').format(_selectedDate)}',                      
-                    ),
-                  ),
-                  FlatButton(
-                    onPressed: _showDatePicker,
-                    child: Text(
-                      'Selecionar uma data',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                        _selectedDate == null ? 'Nenhuma data selecionada'
+                        : 'Data: ${DateFormat('dd/MM/yyyy').format(_selectedDate)}',                      
                       ),
                     ),
-                    textColor: Theme.of(context).primaryColor,
+                    FlatButton(
+                      onPressed: _showDatePicker,
+                      child: Text(
+                        'Selecionar uma data',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      textColor: Theme.of(context).primaryColor,
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  RaisedButton(
+                    child: Text("Nova Transação"),
+                    onPressed: () {
+                      _submitForm();
+                    },
+                    textColor: Theme.of(context).textTheme.button.color,
+                    color: Theme.of(context).primaryColor,
                   ),
                 ],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                RaisedButton(
-                  child: Text("Nova Transação"),
-                  onPressed: () {
-                    _submitForm();
-                  },
-                  textColor: Theme.of(context).textTheme.button.color,
-                  color: Theme.of(context).primaryColor,
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
