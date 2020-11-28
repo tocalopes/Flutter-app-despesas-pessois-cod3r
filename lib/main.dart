2f6c312dd708876ccae8d209ce34d9ade855c5f7
gabriel <gabriellopes112009@gmail.com>
2020-11-27 22:20:35 -0300

    lifecircle of aplication

--- lib/main.dart
+++ lib/main.dart
@@ -50,9 +50,26 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   final List<Transaction> _transactions = [];
   bool _showChart = false;
+
+  @override
+  void initState(){
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state){
+    print(state);
+  }
 
   List<Transaction> get _recentTransactions {
     return _transactions.where((tr) {
