6f92269aa2e172a6d3ce3b6b6acf53324470deb2
gabriel <gabriellopes112009@gmail.com>
2020-11-27 22:40:05 -0300

    turn card_list tatetfull widget

--- lib/components/card_list.dart
+++ lib/components/card_list.dart
@@ -1,13 +1,37 @@
+import 'dart:math';
+
 import 'package:despesa_pessoal/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class CardList extends StatelessWidget {
+class CardList extends StatefulWidget {
   final Transaction transaction;
   final void Function(String) onRemove;
 
   CardList({this.transaction, this.onRemove});
 
+  @override
+  _CardListState createState() => _CardListState();
+}
+
+class _CardListState extends State<CardList> {
+  
+  static const colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.blue,
+    Colors.black
+  ];
+
+  Color _backgroundColor;
+
+  @override
+  void initState() {
+    super.initState();
+    int i = Random().nextInt(5);
+    _backgroundColor = colors[i];
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -19,23 +43,24 @@ class CardList extends StatelessWidget {
       child: ListTile(
         leading: CircleAvatar(
           radius: 30,
+          backgroundColor: _backgroundColor,
           child: Padding(
             padding: const EdgeInsets.all(6),
             child: FittedBox(
-              child: Text('R\$${transaction.value}'),
+              child: Text('R\$${widget.transaction.value}'),
             ),
           ),
         ),
         title: Text(
-          transaction.title,
+          widget.transaction.title,
           style: Theme.of(context).textTheme.headline6,
         ),
         subtitle: Text(
-          DateFormat('d MMM y').format(transaction.date),
+          DateFormat('d MMM y').format(widget.transaction.date),
         ),
         trailing: MediaQuery.of(context).size.width > 480
             ? FlatButton.icon(
-                onPressed: () => onRemove(transaction.id),
+                onPressed: () => widget.onRemove(widget.transaction.id),
                 icon: const Icon(Icons.delete),
                 label: const Text('Excluir'),
                 textColor: Theme.of(context).errorColor,
@@ -43,7 +68,7 @@ class CardList extends StatelessWidget {
             : IconButton(
                 icon: Icon(Icons.delete),
                 color: Theme.of(context).errorColor,
-                onPressed: () => onRemove(transaction.id),
+                onPressed: () => widget.onRemove(widget.transaction.id),
               ),
       ),
     );
